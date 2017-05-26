19fa5c9f33c59cf48e2b2cf450485be7ca4381a2
marspie <marspie.126.com>
2017-05-26 17:37:38 +0800

    cmmit

---
+++ webservice/marspie-webservice-cxf/src/main/java/com/marspie/demo/HelloWorld.java
@@ -0,0 +1,23 @@
+//package com.marspie.demo;
+//
+//import com.marspie.demo.model.RetInfo;
+//import javax.jws.WebMethod;
+//import javax.jws.WebParam;
+//import javax.jws.WebResult;
+//import javax.jws.WebService;
+//
+//@WebService(endpointInterface = "com.marspie.demo.IHelloWorld")
+//public class HelloWorld implements IHelloWorld {
+//
+//	public int add(int num1, int num2) {
+//		return num1 + num2;
+//	}
+//
+//	public RetInfo getRetInfo(String name, int age) {
+//		RetInfo retInfo = new RetInfo();
+//		retInfo.setAge(age);
+//		retInfo.setName(name);
+//		return retInfo;
+//	}
+//
+//}
---
+++ webservice/marspie-webservice-cxf/src/main/java/com/marspie/demo/IHelloWorld.java
@@ -0,0 +1,20 @@
+//package com.marspie.demo;
+//import javax.jws.WebMethod;
+//import javax.jws.WebParam;
+//import javax.jws.WebResult;
+//import javax.jws.WebService;
+//
+//import com.marspie.demo.model.RetInfo; 
+//
+//@WebService
+//public interface IHelloWorld {
+//
+//	@WebMethod(operationName = "add")  
+//    @WebResult(name = "result")  
+//    public int add(@WebParam(name = "num1") int num1,  
+//            @WebParam(name = "num2") int num2);  
+//      
+//    @WebMethod(operationName = "getRetInfo")  
+//    @WebResult(name = "result")  
+//    public RetInfo getRetInfo(@WebParam(name = "name") String name, @WebParam(name = "age") int age);
+//}
