58dd09cacc8b29188ec9fdc03cfa9cbe38f4f2b5
marspie <marspie.126.com>
2017-05-31 10:37:15 +0800

    add designmode

---
+++ designmode/marspie-designmode-observer/src/test/java/com/marspie/demo/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.marspie.demo;
+
+public class SimpleTest    
+{    
+   public static void main(String[] args){    
+      SimpleObservable doc = new SimpleObservable ();    
+      SimpleObserver view = new SimpleObserver (doc);    
+      doc.setData(1);    
+      doc.setData(2);    
+      doc.setData(2);    
+      doc.setData(3);     
+   }    
+}   
